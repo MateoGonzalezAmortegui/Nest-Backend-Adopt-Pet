029a12ee9fd640a68eb2d5c917fa6e58ea310423
Mateo Gonzalez <mateogonza.2000@hotmail.com>
2023-12-11 20:35:09 -0500

    🚧Work in Users (DTO, Entity, Controllers, Services, Module, PipeID, Hashing): All CRUD with errors validations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { MongooseModule } from '@nestjs/mongoose'
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -8,6 +9,7 @@ import { MongooseModule } from '@nestjs/mongoose'
       isGlobal: true,
     }),
     MongooseModule.forRoot(process.env.MONGO_URL),
+    UsersModule,
   ],
 })
 export class AppModule {}
